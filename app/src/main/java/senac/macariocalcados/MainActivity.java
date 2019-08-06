224acfe3eb4e2db095833651026de6f50ea7cfe5
JoseanMarcell <joseanmarcell@gmail.com>
2019-08-06 19:17:20 -0300

    Iniciando programação do mainactivity

--- app/src/main/java/senac/macariocalcados/MainActivity.java
+++ app/src/main/java/senac/macariocalcados/MainActivity.java
@@ -1,17 +1,47 @@
 package senac.macariocalcados;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+
+/* iniciando programação
+
+    private RecyclerView recyclerView;
+    MCAdapter adapter;
+
+    static DatabaseReference myRef;
+    private List<ListaSapatos> listasapato = new ArrayList<>();
+
+    ProgressDialog progressDialog;
+
+    private static boolean FIREBASE_OFFILINE;
+
+    private ValueEventListener ListenerGeral = new ValueEventListener(){
+        @Override
+        public void onDataChange(@NonNull DataSnapShot dataSnapShot ){
+        listasapato.clear();
+            
+
+
+        }
+
+
+        final da programação iniciada
+    } */
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {

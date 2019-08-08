1a3bb6ea3069df6261335a8340cf88be1a0242ab
JoseanMarcell <joseanmarcell@gmail.com>
2019-08-07 21:59:16 -0300

    SapatoAdapter Funcionando Alteração no sapato.class -  acrescentado o this no if. layout promocao

---
+++ app/src/main/java/senac/macariocalcados/item_promocao.java
@@ -0,0 +1,14 @@
+package senac.macariocalcados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class item_promocao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.itens_promocao);
+    }
+}
---
+++ app/src/main/java/senac/macariocalcados/promocoes/promocoes.java
@@ -0,0 +1,19 @@
+package senac.macariocalcados.promocoes;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import senac.macariocalcados.R;
+
+public class promocoes {
+
+    public class imgpromocao extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.itens_promocao);
+        }
+    }
+    
+
+}
